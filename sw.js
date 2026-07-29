8192f6d6cb79affac4fdcf8f46021def072ac071
tetoabdallah-rgb <tetoabdallah@gmail.com>
2026-07-29 18:04:07 +0300

    feat: premium UI/UX overhaul, glassmorphism, animations, modern layout (v23)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'promoter-cache-v25';
+﻿const CACHE_NAME = 'promoter-cache-v26';
 const urlsToCache = [
   './',
   './index.html',
@@ -36,5 +36,6 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
