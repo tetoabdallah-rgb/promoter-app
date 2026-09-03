14f702a310bba812b992272b0d5f4cf85f7038cb
tetoabdallah-rgb <tetoabdallah@gmail.com>
2026-09-03 18:04:47 +0300

    feat: add Phase 3 features (In-App Notifications, Sub-Admin RBAC)

--- sw.js
+++ sw.js
@@ -1,20 +1,13 @@
-self.addEventListener('install', (e) => {
+self.addEventListener('install', () => {
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
     e.waitUntil(
         caches.keys().then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-                return caches.delete(key);
-            }));
-        })
+            return Promise.all(keyList.map((key) => caches.delete(key)));
+        }).then(() => self.registration.unregister())
     );
-    self.registration.unregister().then(() => {
-        self.clients.matchAll().then((clients) => {
-            clients.forEach(client => client.navigate(client.url));
-        });
-    });
 });
 // FORCE CACHE UPDATE 08/13/2026 01:07:06
 // FORCE CACHE UPDATE 2 08/13/2026 01:24:34
